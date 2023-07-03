57277d6fc3c03aec270da943ae6b14de79aa4ea1
Ikedaaaa <matheus-ikeda@hotmail.com>
2023-07-03 08:52:33 -0300

    PROJETO: fileencryption RESUMO: Menu e definição de senha
    Início da implementação do menu, somente com três opções:
    1. Definição/Redefinição de senha
    2. Criptografar/Descriptografar arquivo;
    0. Encerrar programa.
    Porém só foi implementada a opção 1
    
    A definição/redefinição de senha, por enquanto, foi implementada usando SHA-256

--- fileencryption/fileencryption.py
+++ fileencryption/fileencryption.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+import hashlib
 
 def generateKeyFile():
     key = Fernet.generate_key()
@@ -9,6 +10,38 @@ def generateKeyFile():
 def getKeyFromFile():
     with open("key.key", "rb") as keyFile:
         return keyFile.read()
+
+def setPassword():
+    senha1 = input("\nDigite uma senha: ")
+    senha2 = input("Confirme sua senha: ")
+
+    while senha1 != senha2:
+        print("\nAS SENHAS NÃO CONFEREM\n")
+        senha1 = input("Digite uma senha: ")
+        senha2 = input("Confirme sua senha: ")
+
+    with open("password.hash", "wb") as pwdFile:
+        pwdFile.write(hashlib.sha256(senha1.encode("utf-8")).hexdigest().encode())
+
+    print("\nNova senha definida com sucesso\n")
+
+def resetPassword():
+    try:
+        password_hash = "".encode()
+        with open("password.hash", "rb") as pwdFile:
+            password_hash = pwdFile.read()
+
+        password_old = input("\nDigite sua senha antiga: ")
+        password_old_hash = hashlib.sha256(password_old.encode("utf-8")).hexdigest().encode()
+
+        if password_old_hash == password_hash:
+            print("\nRedefinição de senha:")
+            setPassword()
+        else:
+            print("\nAS SENHAS NÃO CONFEREM")
+        
+    except FileNotFoundError:
+        setPassword()
 
 def getFileContent(filename):
     with open(filename, "rb") as readfile:
@@ -35,13 +68,24 @@ if (int(input("Generate new key? (0 - No | 1 - Yes) ")) > 0):
     generateKeyFile()
     print("New key generated\n")
 
-if (int(input("Encrypt file? (0 - No | 1 - Yes) ")) > 0):
+print("\nEscolha uma opção:")
+print("1. Definir/Redefinir senha para criptografia;")
+print("2. Criptografar/Descriptografar arquivo;")
+print("0. Encerrar programa.\n")
+
+opcao = int(input("Opção: "))
+while opcao not in [0, 1, 2]:
+    print("OPÇÃO INVÁLIDA!")
+    opcao = int(input("Opção: "))
+
+if opcao == 1:
+    resetPassword()
+elif opcao == 2:
     key = getKeyFromFile()
     file = input("Type the full path of the file to be encrypted (file included): ")
 
     encrypt(file, key)
 
-if (int(input("Decrypt file? (0 - No | 1 - Yes) ")) > 0):
     key = getKeyFromFile()
     file = input("Type the full path of the file to be decrypted (file included): ")
 
