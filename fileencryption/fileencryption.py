2cd7cc987ebb3276831dfd9617bb88793d7c0a16
Ikedaaaa <matheus-ikeda@hotmail.com>
2023-07-04 17:34:22 -0300

    PROJETO: fileencryption RESUMO: Chave usando senha e salt
    Geração de chave de criptografia usando a senha e salt:
    1. Geração do Salt no processo de definção de senha;
    2. Concatenação do salt à senha para gerar o hash da senha;
    3. Criptografia/Descriptografia verificando antes o hash da senha digitada + salt com o hash armazenado
    4. Caso os hashes baterem, é gerada a chave da criptografia usando a senha digitada + salt armazenado

--- fileencryption/fileencryption.py
+++ fileencryption/fileencryption.py
@@ -1,15 +1,13 @@
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
 import hashlib
+import secrets
+import base64
 
-def generateKeyFile():
-    key = Fernet.generate_key()
-    print("Key type:", type(key))
-    with open("key.key", "wb") as keyFile:
-        keyFile.write(key)
-
-def getKeyFromFile():
-    with open("key.key", "rb") as keyFile:
-        return keyFile.read()
+def getPassword():
+    with open("password.hash", "rb") as pwdFile:
+        return pwdFile.read()
 
 def setPassword():
     senha1 = input("\nDigite uma senha: ")
@@ -20,21 +18,33 @@ def setPassword():
         senha1 = input("Digite uma senha: ")
         senha2 = input("Confirme sua senha: ")
 
+    if (int(input("Generate new salt? (0 - No | 1 - Yes) ")) > 0):
+        saltSize = int(input("Salt size (0 to use standard the size 16): "))
+        salt = generateSalt((saltSize if saltSize > 0 else 16))
+        with open("salt.salt", "wb") as saltFile:
+            saltFile.write(salt)
+    else:
+        salt = loadSalt()
+
+    #print(f"\nSalt: {salt}\n") #Delete after test
+
+    saltedPassword = senha1.encode("utf-8") + salt
+    #print(f"\nSalted Password: {saltedPassword}\n") #Delete after test
+
     with open("password.hash", "wb") as pwdFile:
-        pwdFile.write(hashlib.sha256(senha1.encode("utf-8")).hexdigest().encode())
+        pwdFile.write(hashlib.sha256(saltedPassword).hexdigest().encode())
 
     print("\nNova senha definida com sucesso\n")
 
 def resetPassword():
     try:
-        password_hash = "".encode()
-        with open("password.hash", "rb") as pwdFile:
-            password_hash = pwdFile.read()
+        password_hash = getPassword()
 
         password_old = input("\nDigite sua senha antiga: ")
-        password_old_hash = hashlib.sha256(password_old.encode("utf-8")).hexdigest().encode()
+        salt = loadSalt()
+        #print(f"\nSalt: {salt}\n") #Delete after test
 
-        if password_old_hash == password_hash:
+        if checkPassword(password_old, salt, password_hash):
             print("\nRedefinição de senha:")
             setPassword()
         else:
@@ -43,6 +53,35 @@ def resetPassword():
     except FileNotFoundError:
         setPassword()
 
+def checkPassword(password_input, salt, password_old_hash=""):
+    password_hash = getPassword() if password_old_hash == "" else password_old_hash
+    #print(f"\nPassword Hash: {password_hash}\n") #Delete after test
+    #print(f"\nPassword Input: {password_input}\n") #Delete after test
+    #print(f"\nSalt: {salt}\n") #Delete after test
+    salted_password = password_input.encode("utf-8") + salt
+    #print(f"\nSalted Password: {salted_password}\n") #Delete after test
+    password_input_hash = hashlib.sha256(salted_password).hexdigest().encode()
+    #print(f"\nPassword Input Hash: {password_input_hash}\n") #Delete after test
+    return password_input_hash == password_hash
+
+def generateSalt(size):
+    return secrets.token_bytes(size)
+
+def loadSalt():
+    with open("salt.salt", "rb") as saltFile:
+        return saltFile.read()
+    
+def deriveKey(salt, password):
+    kdf = Scrypt(salt=salt, length=32, n=2**20, r=8, p=1)
+    return kdf.derive(password.encode())
+
+def generateKey(password, salt): #, salt_size, generateSalt
+    #generate key from salt and password and encode it using Base 64
+    print("\nDeriving Criptography key from password\n")
+    derived_key = deriveKey(salt, password)
+    #print(f"\nDerived Key: {derived_key}\n")
+    return base64.urlsafe_b64encode(derived_key)
+
 def getFileContent(filename):
     with open(filename, "rb") as readfile:
         return readfile.read()
@@ -50,23 +89,26 @@ def getFileContent(filename):
 def setFileContent(filename, data):
     with open(filename, "wb") as writefile:
         writefile.write(data)
-    print(f"Saved changes to file {filename}\n")
 
-def encrypt(filepath, key):
-    cryptographyObject = Fernet(key)
-
+def encrypt(filepath, cryptographyObject):
     encrypted_data = cryptographyObject.encrypt(getFileContent(filepath))
     setFileContent(filepath, encrypted_data)
+    print(f"File {filepath} ENCRYPTED\n")
 
-def decrypt(filepath, key):
-    cryptographyObject = Fernet(key)
-
-    decrypted_data = cryptographyObject.decrypt(getFileContent(filepath))
-    setFileContent(filepath, decrypted_data)
-
-if (int(input("Generate new key? (0 - No | 1 - Yes) ")) > 0):
-    generateKeyFile()
-    print("New key generated\n")
+def decrypt(filepath, pwd):
+    salt = loadSalt()
+    if checkPassword(pwd, salt):
+        key = generateKey(pwd, salt)
+        #print(f"\nCriptography Key: {key}\n")
+        cryptographyObject = Fernet(key)
+        try:
+            decrypted_data = cryptographyObject.decrypt(getFileContent(filepath))
+            setFileContent(filepath, decrypted_data)
+            print(f"File {filepath} DECRYPTED\n")
+        except:
+            encrypt(filepath, cryptographyObject)
+    else:
+        print("\nWARNING: Wrong Password\n")
 
 print("\nEscolha uma opção:")
 print("1. Definir/Redefinir senha para criptografia;")
@@ -81,14 +123,8 @@ while opcao not in [0, 1, 2]:
 if opcao == 1:
     resetPassword()
 elif opcao == 2:
-    key = getKeyFromFile()
-    file = input("Type the full path of the file to be encrypted (file included): ")
-
-    encrypt(file, key)
-
-    key = getKeyFromFile()
-    file = input("Type the full path of the file to be decrypted (file included): ")
-
-    decrypt(file, key)
+    file = input("Type the full path of the file to be encrypted/decrypted (file included): ")
+    pwd = input("Type your password: ")
+    decrypt(file, pwd)
 
 print("\n*************** End of program ***************")
