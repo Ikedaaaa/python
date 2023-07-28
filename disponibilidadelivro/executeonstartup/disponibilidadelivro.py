f8265d50450962edf62b9e3394fe8b0f258e9f2b
Ikedaaaa <matheus-ikeda@hotmail.com>
2023-07-27 22:21:15 -0300

    PROJETO: disponibilidadelivro RESUMO: Dupla requisição
    Requisição para duas URLs:
    - Uma requisição é para a URL desejada;
    - A outra é um teste em que o retorno de disponibilidade = True

--- disponibilidadelivro/executeonstartup/disponibilidadelivro.py
+++ disponibilidadelivro/executeonstartup/disponibilidadelivro.py
@@ -1,54 +1,88 @@
 import requests
-import time
-from datetime import datetime
+import logging
+
+def getRequestExceptionString(url, data):
+    return f"Exception occured during request to URL:\n{url}\nWith request data:\n{data}\n"
+
+def isDifferentPrice(differentPrices, newPrice):
+    for differentPrice in differentPrices:
+        if newPrice == differentPrice[1]:
+            return False
+    return True 
+
+def logResponseData(response_data):
+    prices = []
+    different_prices = []
+
+    status_code = response_data.status_code
+    response_data_json = response_data.json()
+
+    is_available_for_sale = response_data_json.get("cart").get("offer").get("is_available_for_sale")
+    availability = response_data_json.get("cart").get("offer").get("availability")
+    offer_items = response_data_json.get("cart").get("offer").get("offer_items")[0]
+
+    prices.append(['cart.offer.price', response_data_json.get("cart").get("offer").get("price")])
+    prices.append(['cart.offer.offer_price.price', response_data_json.get("cart").get("offer").get("offer_price")[0].get("price")])
+    prices.append(['cart.offer.offer_items.price', offer_items.get("price")])
+    prices.append(['cart.offer.offer_items.plan_data.price', offer_items.get("plan_data").get("price")])
+
+    for idx, price in enumerate(prices):
+        if idx == 0:
+            different_prices.append(price)
+        else:
+            if isDifferentPrice(different_prices, price[1]):
+                different_prices.append(price)
+
+    bookName = offer_items.get("plan_data").get("product_data").get("name")
+
+    print("\n\nStatus Code:",status_code)
+
+    print(f"Book Name: {bookName}\n")
+
+    if len(different_prices) == 1:
+        print("PRICE")
+        print(f"R${'%.2f' % different_prices[0][1]}")
+    else:
+        print("PRICES")
+        for different_price in different_prices:
+            print(f"{different_price[0]}: R${'%.2f' % different_price[1]}")
+
+    print(f"Is Available For Sale: {is_available_for_sale}\n")
+
+    print(f"AVAILABILITY:")
+    print(f"Error Code: {availability.get('error_code')}")
+    print(f"Available: {availability.get('available')}\n\n")
+
 
 # Define new data to create
 new_data = {
     "payment_method": "creditCard"
 }
+'''
+filename='disponibilidadelivro.log',
+    filemode='a',
+'''
+logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 
-# The API endpoint to communicate with
-url_post = "https://store-api.empiricus.com.br/commerce/v1/storefront/livro-criptomoedas-avulso"
-#Exemplo URL Available = True
-#"https://store-api.empiricus.com.br/commerce/v1/storefront/livro-cripto-wars-avulso"
+#URL "Available = False" at the moment
+url_criptomoedas = "https://store-api.empiricus.com.br/commerce/v1/storefront/livro-criptomoedas-avulso"
+#Exemplo URL "Available = True" at the moment
+url_criptowars = "https://store-api.empiricus.com.br/commerce/v1/storefront/livro-cripto-wars-avulso"
 
-print(f"Hora antes da requisição: {datetime.now()}")
-t1 = time.perf_counter_ns()
+# Request to Criptomoedas book URL
+try:
+    logging.info(f"Making request to URL {url_criptomoedas}")
+    response_criptomoedas = requests.post(url_criptomoedas, json=new_data)
+except Exception as e:
+    logging.exception(getRequestExceptionString(url_criptomoedas, new_data))
 
-# A POST request to the API
-post_response = requests.post(url_post, json=new_data)
+logResponseData(response_criptomoedas)
 
-t2 = time.perf_counter_ns()
-print(f"Hora depois da requisição: {datetime.now()}")
-print(f"Duração da requisição: {(t2 - t1)/1000000000} segundos\n")
+# Request to Criptowars book URL
+try:
+    logging.info(f"Making request to URL {url_criptowars}")
+    response_criptowars = requests.post(url_criptowars, json=new_data)
+except Exception as e:
+    logging.exception(getRequestExceptionString(url_criptowars, new_data))
 
-# Print status code from original response (not JSON)
-print("Status Code:",post_response.status_code)
-
-# Print the response
-post_response_json = post_response.json()
-
-is_available_for_sale = post_response_json.get("cart").get("offer").get("is_available_for_sale")
-availability = post_response_json.get("cart").get("offer").get("availability")
-offer_items = post_response_json.get("cart").get("offer").get("offer_items")[0]
-
-priceUnder_offer = post_response_json.get("cart").get("offer").get("price")
-priceUnder_offer_price = post_response_json.get("cart").get("offer").get("offer_price")[0].get("price")
-priceUnder_offer_items = offer_items.get("price")
-priceUnder_plan_data = offer_items.get("plan_data").get("price")
-
-bookName = offer_items.get("plan_data").get("product_data").get("name")
-
-print(f"Book Name: {bookName}\n")
-
-print("PRICES:")
-print(f"cart.offer.price: R${'%.2f' % priceUnder_offer}")
-print(f"cart.offer.offer_price.price: R${'%.2f' % priceUnder_offer_price}")
-print(f"cart.offer.offer_items.price: R${'%.2f' % priceUnder_offer_items}")
-print(f"cart.offer.offer_items.plan_data.price: R${'%.2f' % priceUnder_plan_data}\n")
-
-print(f"Is Available For Sale: {is_available_for_sale}\n")
-
-print(f"AVAILABILITY:")
-print(f"Error Code: {availability.get('error_code')}")
-print(f"Available: {availability.get('available')}")
+logResponseData(response_criptowars)
